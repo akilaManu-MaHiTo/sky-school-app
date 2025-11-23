31b96127f69f59b317df709a6147ed4fd0af5788
akilaManu <akilamanujith@gmail.com>
2025-11-23 14:26:24 +0530

    Update permission sections and keys for dashboard and management features

--- src/views/Administration/SectionList.ts
+++ src/views/Administration/SectionList.ts
@@ -17,10 +17,49 @@ export const PermissionSectionsMap: PermissionSection[] = [
     ],
   },
   {
+    mainSection: "Dashboard",
+    subSections: [
+      {
+        name: "Student/Parent Dashboard",
+        key: "STUDENT_PARENT_DASHBOARD",
+        permissionsExists: {
+          VIEW: true,
+          CREATE: false,
+          EDIT: false,
+          DELETE: false,
+        },
+      },
+      {
+        name: "Teachers Dashboard",
+        key: "TEACHERS_DASHBOARD",
+        permissionsExists: {
+          VIEW: true,
+          CREATE: false,
+          EDIT: false,
+          DELETE: false,
+        },
+      },
+      {
+        name: "Management Dashboard",
+        key: "MANAGEMENT_DASHBOARD",
+        permissionsExists: {
+          VIEW: true,
+          CREATE: false,
+          EDIT: false,
+          DELETE: false,
+        },
+      },
+    ],
+  },
+  {
     mainSection: "Administration",
     subSections: [
       {
-        name: "Administration > Users",
+        break: true,
+        name: "User Management",
+      },
+      {
+        name: "User Management > All Users",
         key: "ADMIN_USERS",
         permissionsExists: {
           VIEW: true,
@@ -30,7 +69,7 @@ export const PermissionSectionsMap: PermissionSection[] = [
         },
       },
       {
-        name: "Administration > Access Management",
+        name: "User Management > Access Management",
         key: "ADMIN_ACCESS_MNG",
         permissionsExists: {
           VIEW: true,
@@ -39,18 +78,53 @@ export const PermissionSectionsMap: PermissionSection[] = [
           DELETE: true,
         },
       },
+      {
+        break: true,
+        name: "School Management",
+      },
+      {
+        name: "School Management > School Settings",
+        key: "SCHOOL_SETTINGS",
+        permissionsExists: {
+          VIEW: true,
+          CREATE: true,
+          EDIT: true,
+          DELETE: true,
+        },
+      },
+      { break: true, name: "Staff Management" },
+      {
+        name: "Add Class Teacher",
+        key: "ADD_CLASS_TEACHER",
+        permissionsExists: {
+          VIEW: true,
+          CREATE: true,
+          EDIT: true,
+          DELETE: true,
+        },
+      },
+      {
+        break: true,
+        name: "Student Management",
+      },
+      {
+        name: "Student Promotion",
+        key: "STUDENT_PROMOTION",
+        permissionsExists: {
+          VIEW: true,
+          CREATE: true,
+          EDIT: true,
+          DELETE: true,
+        },
+      },
     ],
   },
   {
-    mainSection: "Design",
+    mainSection: "Reports",
     subSections: [
       {
-        break: true,
-        name: "Components",
-      },
-      {
-        name: "Accordion & Divider",
-        key: "COMPONENTS_ACCORDION_DIVIDER",
+        name: "Student/Parent Reports",
+        key: "STUDENT_PARENT_REPORTS",
         permissionsExists: {
           VIEW: true,
           CREATE: false,
@@ -59,8 +133,8 @@ export const PermissionSectionsMap: PermissionSection[] = [
         },
       },
       {
-        name: "Image Designs",
-        key: "COMPONENTS_IMAGE_DESIGNS",
+        name: "Teacher Reports",
+        key: "TEACHER_REPORTS",
         permissionsExists: {
           VIEW: true,
           CREATE: false,
@@ -69,62 +143,8 @@ export const PermissionSectionsMap: PermissionSection[] = [
         },
       },
       {
-        name: "Tab Panel",
-        key: "COMPONENTS_TAB_PANEL",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: false,
-          EDIT: false,
-          DELETE: false,
-        },
-      },
-      {
-        name: "Under Development",
-        key: "COMPONENTS_UNDER_DEVELOPMENT",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: false,
-          EDIT: false,
-          DELETE: false,
-        },
-      },
-      {
-        break: true,
-        name: "Input Fields",
-      },
-      {
-        name: "Autocomplete",
-        key: "INPUT_FIELDS_AUTOCOMPLETE",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: false,
-          EDIT: false,
-          DELETE: false,
-        },
-      },
-      {
-        name: "Text Fields",
-        key: "INPUT_FIELDS_TEXT_FIELDS",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: false,
-          EDIT: false,
-          DELETE: false,
-        },
-      },
-      {
-        name: "Date Pickers",
-        key: "INPUT_FIELDS_DATE_PICKERS",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: false,
-          EDIT: false,
-          DELETE: false,
-        },
-      },
-      {
-        name: "Other Input Fields",
-        key: "INPUT_FIELDS_OTHER_INPUTS",
+        name: "Management Staff Reports",
+        key: "MANAGEMENT_STAFF_REPORTS",
         permissionsExists: {
           VIEW: true,
           CREATE: false,
@@ -135,55 +155,11 @@ export const PermissionSectionsMap: PermissionSection[] = [
     ],
   },
   {
-    mainSection: "Sample CRUD",
+    mainSection: "Academics",
     subSections: [
       {
-        break: true,
-        name: "Chemical MNG",
-      },
-      {
-        name: "Dashboard",
-        key: "CHEMICAL_MNG_DASHBOARD",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: false,
-          EDIT: false,
-          DELETE: false,
-        },
-      },
-      {
-        name: "Request Register",
-        key: "CHEMICAL_MNG_REQUEST_REGISTER",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: true,
-          EDIT: true,
-          DELETE: true,
-        },
-      },
-      {
-        name: "Purchase & Inventory",
-        key: "CHEMICAL_MNG_PURCHASE_INVENTORY",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: true,
-          EDIT: true,
-          DELETE: true,
-        },
-      },
-      {
-        name: "Transaction",
-        key: "CHEMICAL_MNG_TRANSACTION",
-        permissionsExists: {
-          VIEW: true,
-          CREATE: true,
-          EDIT: true,
-          DELETE: true,
-        },
-      },
-      {
-        name: "Assigned Tasks",
-        key: "CHEMICAL_MNG_ASSIGNED_TASKS",
+        name: "Add Marks",
+        key: "ADD_MARKS",
         permissionsExists: {
           VIEW: true,
           CREATE: true,
@@ -223,42 +199,43 @@ export interface PermissionsExists {
 export enum PermissionKeys {
   //Insight
   INSIGHT_VIEW = "INSIGHT_VIEW",
-  // Administration
+  // Dashboard
+  STUDENT_PARENT_DASHBOARD_VIEW = "STUDENT_PARENT_DASHBOARD_VIEW",
+  TEACHERS_DASHBOARD_VIEW = "TEACHERS_DASHBOARD_VIEW",
+  MANAGEMENT_DASHBOARD_VIEW = "MANAGEMENT_DASHBOARD_VIEW",
+  // Administration > User Management > All Users
   ADMIN_USERS_VIEW = "ADMIN_USERS_VIEW",
   ADMIN_USERS_EDIT = "ADMIN_USERS_EDIT",
   ADMIN_USERS_DELETE = "ADMIN_USERS_DELETE",
+  // Administration > User Management > Access Management
   ADMIN_ACCESS_MNG_VIEW = "ADMIN_ACCESS_MNG_VIEW",
   ADMIN_ACCESS_MNG_CREATE = "ADMIN_ACCESS_MNG_CREATE",
   ADMIN_ACCESS_MNG_EDIT = "ADMIN_ACCESS_MNG_EDIT",
   ADMIN_ACCESS_MNG_DELETE = "ADMIN_ACCESS_MNG_DELETE",
-  // Design
-  COMPONENTS_ACCORDION_DIVIDER_VIEW = "COMPONENTS_ACCORDION_DIVIDER_VIEW",
-  COMPONENTS_IMAGE_DESIGNS_VIEW = "COMPONENTS_IMAGE_DESIGNS_VIEW",
-  COMPONENTS_TAB_PANEL_VIEW = "COMPONENTS_TAB_PANEL_VIEW",
-  COMPONENTS_UNDER_DEVELOPMENT_VIEW = "COMPONENTS_UNDER_DEVELOPMENT_VIEW",
-  // Input Fields
-  INPUT_FIELDS_AUTOCOMPLETE_VIEW = "INPUT_FIELDS_AUTOCOMPLETE_VIEW",
-  INPUT_FIELDS_TEXT_FIELDS_VIEW = "INPUT_FIELDS_TEXT_FIELDS_VIEW",
-  INPUT_FIELDS_DATE_PICKERS_VIEW = "INPUT_FIELDS_DATE_PICKERS_VIEW",
-  INPUT_FIELDS_OTHER_INPUTS_VIEW = "INPUT_FIELDS_OTHER_INPUTS_VIEW",
-  // Chemical MNG
-  CHEMICAL_MNG_DASHBOARD_VIEW = "CHEMICAL_MNG_DASHBOARD_VIEW",
-  CHEMICAL_MNG_REQUEST_REGISTER_VIEW = "CHEMICAL_MNG_REQUEST_REGISTER_VIEW",
-  CHEMICAL_MNG_REQUEST_REGISTER_CREATE = "CHEMICAL_MNG_REQUEST_REGISTER_CREATE",
-  CHEMICAL_MNG_REQUEST_REGISTER_EDIT = "CHEMICAL_MNG_REQUEST_REGISTER_EDIT",
-  CHEMICAL_MNG_REQUEST_REGISTER_DELETE = "CHEMICAL_MNG_REQUEST_REGISTER_DELETE",
-  CHEMICAL_MNG_PURCHASE_INVENTORY_VIEW = "CHEMICAL_MNG_PURCHASE_INVENTORY_VIEW",
-  CHEMICAL_MNG_PURCHASE_INVENTORY_CREATE = "CHEMICAL_MNG_PURCHASE_INVENTORY_CREATE",
-  CHEMICAL_MNG_PURCHASE_INVENTORY_EDIT = "CHEMICAL_MNG_PURCHASE_INVENTORY_EDIT",
-  CHEMICAL_MNG_PURCHASE_INVENTORY_DELETE = "CHEMICAL_MNG_PURCHASE_INVENTORY_DELETE",
-  CHEMICAL_MNG_TRANSACTION_VIEW = "CHEMICAL_MNG_TRANSACTION_VIEW",
-  CHEMICAL_MNG_TRANSACTION_CREATE = "CHEMICAL_MNG_TRANSACTION_CREATE",
-  CHEMICAL_MNG_TRANSACTION_EDIT = "CHEMICAL_MNG_TRANSACTION_EDIT",
-  CHEMICAL_MNG_TRANSACTION_DELETE = "CHEMICAL_MNG_TRANSACTION_DELETE",
-  CHEMICAL_MNG_ASSIGNED_TASKS_VIEW = "CHEMICAL_MNG_ASSIGNED_TASKS_VIEW",
-  CHEMICAL_MNG_ASSIGNED_TASKS_CREATE = "CHEMICAL_MNG_ASSIGNED_TASKS_CREATE",
-  CHEMICAL_MNG_ASSIGNED_TASKS_EDIT = "CHEMICAL_MNG_ASSIGNED_TASKS_EDIT",
-  CHEMICAL_MNG_ASSIGNED_TASKS_DELETE = "CHEMICAL_MNG_ASSIGNED_TASKS_DELETE",
+  // Administration > School Management > School Settings
+  SCHOOL_SETTINGS_VIEW = "SCHOOL_SETTINGS_VIEW",
+  SCHOOL_SETTINGS_CREATE = "SCHOOL_SETTINGS_CREATE",
+  SCHOOL_SETTINGS_EDIT = "SCHOOL_SETTINGS_EDIT",
+  SCHOOL_SETTINGS_DELETE = "SCHOOL_SETTINGS_DELETE",
+  // Administration > Staff Management > Add Class Teacher
+  ADD_CLASS_TEACHER_VIEW = "ADD_CLASS_TEACHER_VIEW",
+  ADD_CLASS_TEACHER_CREATE = "ADD_CLASS_TEACHER_CREATE",
+  ADD_CLASS_TEACHER_EDIT = "ADD_CLASS_TEACHER_EDIT",
+  ADD_CLASS_TEACHER_DELETE = "ADD_CLASS_TEACHER_DELETE",
+  // Administration > Student Management > Student Promotion
+  STUDENT_PROMOTION_VIEW = "STUDENT_PROMOTION_VIEW",
+  STUDENT_PROMOTION_CREATE = "STUDENT_PROMOTION_CREATE",
+  STUDENT_PROMOTION_EDIT = "STUDENT_PROMOTION_EDIT",
+  STUDENT_PROMOTION_DELETE = "STUDENT_PROMOTION_DELETE",
+  // Reports
+  STUDENT_PARENT_REPORTS_VIEW = "STUDENT_PARENT_REPORTS_VIEW",
+  TEACHER_REPORTS_VIEW = "TEACHER_REPORTS_VIEW",
+  MANAGEMENT_STAFF_REPORTS_VIEW = "MANAGEMENT_STAFF_REPORTS_VIEW",
+  // Academics
+  ADD_MARKS_VIEW = "ADD_MARKS_VIEW",
+  ADD_MARKS_CREATE = "ADD_MARKS_CREATE",
+  ADD_MARKS_EDIT = "ADD_MARKS_EDIT",
+  ADD_MARKS_DELETE = "ADD_MARKS_DELETE",
 }
 
 // Create the Zod schema using the enum values
@@ -283,40 +260,42 @@ export const defaultAdminPermissions = Object.values(PermissionKeys).reduce(
 export const defaultViewerPermissions: PermissionKeysObject = {
   // Insight
   INSIGHT_VIEW: true,
-  // Administration
+  // Dashboard
+  STUDENT_PARENT_DASHBOARD_VIEW: true,
+  TEACHERS_DASHBOARD_VIEW: true,
+  MANAGEMENT_DASHBOARD_VIEW: true,
+  // Administration > User Management > All Users
   ADMIN_USERS_VIEW: true,
   ADMIN_USERS_EDIT: true,
   ADMIN_USERS_DELETE: true,
+  // Administration > User Management > Access Management
   ADMIN_ACCESS_MNG_VIEW: true,
   ADMIN_ACCESS_MNG_CREATE: true,
   ADMIN_ACCESS_MNG_EDIT: true,
   ADMIN_ACCESS_MNG_DELETE: true,
-  // Design
-  COMPONENTS_ACCORDION_DIVIDER_VIEW: true,
-  COMPONENTS_IMAGE_DESIGNS_VIEW: true,
-  COMPONENTS_TAB_PANEL_VIEW: true,
-  COMPONENTS_UNDER_DEVELOPMENT_VIEW: true,
-  // Input Fields
-  INPUT_FIELDS_AUTOCOMPLETE_VIEW: true,
-  INPUT_FIELDS_TEXT_FIELDS_VIEW: true,
-  INPUT_FIELDS_DATE_PICKERS_VIEW: true,
-  INPUT_FIELDS_OTHER_INPUTS_VIEW: true,
-  // Chemical MNG
-  CHEMICAL_MNG_DASHBOARD_VIEW: true,
-  CHEMICAL_MNG_REQUEST_REGISTER_VIEW: true,
-  CHEMICAL_MNG_REQUEST_REGISTER_CREATE: true,
-  CHEMICAL_MNG_REQUEST_REGISTER_EDIT: true,
-  CHEMICAL_MNG_REQUEST_REGISTER_DELETE: true,
-  CHEMICAL_MNG_PURCHASE_INVENTORY_VIEW: true,
-  CHEMICAL_MNG_PURCHASE_INVENTORY_CREATE: true,
-  CHEMICAL_MNG_PURCHASE_INVENTORY_EDIT: true,
-  CHEMICAL_MNG_PURCHASE_INVENTORY_DELETE: true,
-  CHEMICAL_MNG_TRANSACTION_VIEW: true,
-  CHEMICAL_MNG_TRANSACTION_CREATE: true,
-  CHEMICAL_MNG_TRANSACTION_EDIT: true,
-  CHEMICAL_MNG_TRANSACTION_DELETE: true,
-  CHEMICAL_MNG_ASSIGNED_TASKS_VIEW: true,
-  CHEMICAL_MNG_ASSIGNED_TASKS_CREATE: true,
-  CHEMICAL_MNG_ASSIGNED_TASKS_EDIT: true,
-  CHEMICAL_MNG_ASSIGNED_TASKS_DELETE: true,
+  // Administration > School Management > School Settings
+  SCHOOL_SETTINGS_VIEW: true,
+  SCHOOL_SETTINGS_CREATE: true,
+  SCHOOL_SETTINGS_EDIT: true,
+  SCHOOL_SETTINGS_DELETE: true,
+  // Administration > Staff Management > Add Class Teacher
+  ADD_CLASS_TEACHER_VIEW: true,
+  ADD_CLASS_TEACHER_CREATE: true,
+  ADD_CLASS_TEACHER_EDIT: true,
+  ADD_CLASS_TEACHER_DELETE: true,
+  // Administration > Student Management > Student Promotion
+  STUDENT_PROMOTION_VIEW: true,
+  STUDENT_PROMOTION_CREATE: true,
+  STUDENT_PROMOTION_EDIT: true,
+  STUDENT_PROMOTION_DELETE: true,
+  // Reports
+  STUDENT_PARENT_REPORTS_VIEW: true,
+  TEACHER_REPORTS_VIEW: true,
+  MANAGEMENT_STAFF_REPORTS_VIEW: true,
+  // Academics
+  ADD_MARKS_VIEW: true,
+  ADD_MARKS_CREATE: true,
+  ADD_MARKS_EDIT: true,
+  ADD_MARKS_DELETE: true,
+
 };
